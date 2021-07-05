d65b3cf33b95dd5f848b464374a5aa59d34fc4fe
Abdiel Martinez <aabdiel1998@gmail.com>
2021-07-04 22:18:08 -0600

    🔥 removed dist files

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-export { default as useFetch } from "./useFetch";
-export { default as useLazyFetch } from "./useLazyFetch";
-export { default as setGlobals } from './setGlobals';
-export { default as useFetchCache } from './useFetchCache';
-export { default as useLazyFetchCache } from './useLazyFetchCache';
-export { default as CacheProvider } from './CacheContext';
-export { default as useSearch } from './useSearch';
-export { default as usePromise } from './usePromise';
--- dist/useFetch.d.ts
+++
@@ -1,9 +0,0 @@
-import { UseLazyFetchProps, RequestUseLazyFetch } from './useLazyFetch';
-export declare type UseFetchProps<T> = UseLazyFetchProps<T>;
-export declare type RequestUseFetch = RequestUseLazyFetch;
-declare const _default: <T = any>(props?: Partial<UseFetchProps<T>> | undefined) => readonly [import("./useLazyFetch").State<T>, (request?: RequestUseLazyFetch) => Promise<void>];
-/**
- * useFetch
- * @param initialSettings
- */
-export default _default;
